38dbe3f98ba9a6028abef8161de835f9163a0b8f
Raul Romero <rromero3@gmail.com>
2015-10-27 02:18:43 +0000

    Odoo open erp bitnami source code master

---
+++ bin/unit2.py
@@ -0,0 +1,4 @@
+#!/opt/bitnami/python/bin/python
+# EASY-INSTALL-SCRIPT: 'unittest2==0.5.1','unit2.py'
+__requires__ = 'unittest2==0.5.1'
+__import__('pkg_resources').run_script('unittest2==0.5.1', 'unit2.py')
