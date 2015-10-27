34a29d1ef12459f9dcb1902b6b383eafa903b93c
Raul Romero <rromero3@gmail.com>
2015-10-27 18:34:22 -0400

    created empro fleet hours meter module
    added files and updated configuration file to look at custom_modules
    folder

---
+++ custom_modules/empro_fleet_hoursmeter/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Empro fleet hoursmeter integration",
+    'version': "1.0",
+    'author': "Raul Romero",
+    'category': "Tools",
+    'depends': ['fleet'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+}
